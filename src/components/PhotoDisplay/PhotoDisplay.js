274e0386eba46f5b0e70d95420690afbd620593e
theseanco <theseancotterill@gmail.com>
2018-12-11 13:38:04 +0000

    images importing in bulk, need to map over array of images

--- src/components/PhotoDisplay/PhotoDisplay.js
+++ src/components/PhotoDisplay/PhotoDisplay.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CrossfadeImage from 'react-crossfade-image'
 import './PhotoDisplay.css';
 import ReactCSSTransitionReplace from 'react-css-transition-replace';
 //sourced from here: https://codepen.io/jjmartucci/pen/avqPBW
@@ -15,6 +14,18 @@ But i don't quite know how
 
 */
 
+//function to import all, used with require.context to import directories
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+
+//import all images
+const imageUrls = importAll(require.context('../../assets/images/albumCovers', false, /\.(png|jpg|svg)$/));
+console.log(Object.keys(imageUrls));
+
 class PhotoDisplay extends Component {
 
   render() {
@@ -23,13 +34,15 @@ class PhotoDisplay extends Component {
       pictureNumber,
     } = this.props;
 
-  let imagesrc = require(`../../assets/images/${images[pictureNumber]}`);
+  // let imagesrc = require(`../../assets/images/${images[pictureNumber]}`);
   // let imagesrc = `https://raw.githubusercontent.com/theseanco/co34pt_timeline/master/covers-cropped/cover${pictureNumber}.jpg`;
-  let divStyle = {backgroundImage: `url(${imagesrc})`};
+ let divStyle = {backgroundImage: `url(${images[2]})`};
 
   return (
-        <div className="imageDiv" style={divStyle}>
-        </div>
+         <div className="imageDiv">
+         //next job is to run a map function over all of the images available. 
+          <img src={imageUrls['cover1.jpg']} />
+         </div>
       )
 }
 }
