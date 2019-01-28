af168256db7cc7c7c7eee1378da5190e22bbef36
theseanco <theseancotterill@gmail.com>
2019-01-28 11:08:38 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/co34pt_timeline/precache-manifest.7503df20324220331b8d2b337fc29676.js"
+  "/co34pt_timeline/precache-manifest.4aa57606fe97e7e9117d76631815abdc.js"
 );
 
 workbox.clientsClaim();
